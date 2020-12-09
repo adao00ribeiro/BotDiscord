3c31a8246749bf6189a94ce641ac70bfbc3705ee
adao00ribeiro <adao-eduardo@hotmail.com>
2020-12-09 16:26:09 -0300

    kenzy

---
+++ commands/kenzy.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'kenzy',
+    description: 'Salva Tags',
+    execute(message){
+        
+        message.channel.send("https://cdn.discordapp.com/attachments/762762277584633878/786309144868421642/unknown.png");
+    },
+    
+};
